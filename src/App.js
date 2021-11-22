325094cd0fb5ed02f20880958230957aa9878890
Javi Sanchez <javi@Javis-MacBook-Pro.local>
2021-11-22 17:41:33 +0000

    Feat: Adds configurable title and description, and adds margin to card div

--- src/App.js
+++ src/App.js
@@ -8,7 +8,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Card />
+        <Card
+          title="Test title"
+          description="Aliquip eu ea esse excepteur sint elit culpa et."
+        />
+        <Card title="Test1" description="description 1" />
       </header>
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,11 +2,11 @@ import React from "react";
 
 import classes from "./Card.module.css";
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div className={classes.card}>
-      <h1 className={classes.title}>Title</h1>
-      <p className={classes.p}>Place your description here</p>
+      <h1 className={classes.title}>{props.title}</h1>
+      <p className={classes.p}>{props.description}</p>
     </div>
   );
 };
