fd0f9d82cfadaaa9570d252e2bf8dd758f6d428d
Javi Sanchez <javi@Javis-MacBook-Pro.local>
2021-11-22 17:16:05 +0000

    Feat: Adds basic card (without styling)

---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div>
+      <h1>Title</h1>
+      <p>Place your description here</p>
+    </div>
+  );
+};
+
+export default Card;
