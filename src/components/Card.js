bda4ac0caaa4c7d50da3cf02d4a7433d5c5770d0
Javi Sanchez <javi@Javis-MacBook-Pro.local>
2021-11-22 17:26:32 +0000

    Feat: Adds initial styling to card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,12 @@
 import React from "react";
 
+import classes from "./Card.module.css";
+
 const Card = () => {
   return (
-    <div>
-      <h1>Title</h1>
-      <p>Place your description here</p>
+    <div className={classes.card}>
+      <h1 className={classes.title}>Title</h1>
+      <p className={classes.p}>Place your description here</p>
     </div>
   );
 };
